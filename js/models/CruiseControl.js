5aaa849d9b539c1ce6c1d5cc478cc65df7bcd53f
matias morant <matias.morant.mm@gmail.com>
2025-09-30 22:24:38 -0300

    Update CruiseControl.js

--- js/models/CruiseControl.js
+++ js/models/CruiseControl.js
@@ -3,12 +3,9 @@ if (typeof Models === 'undefined') var Models = {};
 
 Models.CruiseControl = function(params)
 {
-    var nVars = Object.keys(this.vars).length;
-    for(var i = 0; i < nVars; i++)
-    {
-        var key = Object.keys(this.vars)[i];
-        this[key] = (typeof params[key] == 'undefined')?this.vars[key]:params[key];
-    }
+    Object.keys(this.vars).forEach(key => {
+      this[key] = (typeof params[key] === 'undefined') ? this.vars[key] : params[key];
+    });
 }
 
 Models.CruiseControl.prototype.vars = 
